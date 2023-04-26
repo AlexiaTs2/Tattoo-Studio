5a8c1a536998002994c745e18721ff13e9f97dac
AlexiaTs <alexia.tcvetanova@gmail.com>
2023-04-26 19:43:50 +0300

    000

--- About/about.php
+++ About/about.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../Nav/nav.css">
     <link rel="shortcut icon" type= "x-icon" href="../Images/Logo.png">
     <link rel="stylesheet" type="text/css" href="aboutStyle.css">
-    <title>About             uoougugfiyufguohio</title>
+    <title>About</title>
 </head>
 <body>
     <header>
