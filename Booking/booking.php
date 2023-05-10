de54b27ed62dab932125409e72817f29acf13d58
TsankoDokov <tsankod05g@gmail.com>
2023-05-10 16:04:21 +0300

    booking form in database

--- Booking/booking.php
+++ Booking/booking.php
@@ -1,3 +1,44 @@
+<?php
+// Връзка към базата данни
+    $db_host = "localhost"; // Името на хоста, където се намира базата данни
+    $db_name = "studio"; // Името на базата данни
+    $db_user = "root"; // Потребителско име за достъп до базата данни
+    $db_pass = "1234"; // Парола за достъп до базата данни
+
+// Създаване на връзка към базата данни
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// Проверка за грешки при връзката
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Извличане на информацията от формата
+$visitor_name = $_POST['visitor_name'];
+$visitor_email = $_POST['visitor_email'];
+$visitor_phone = $_POST['visitor_phone'];
+$checkin_date = $_POST['checkin_date'];
+$checkin_hour = $_POST['checkin_hour'];
+$artist = $_POST['artist'];
+
+
+// Записване на информацията в базата данни
+$sql = "INSERT INTO booking (visitor_name, visitor_email, visitor_phone, checkin_date, checkin_hour, artist) 
+        VALUES ('$visitor_name', '$visitor_email', '$visitor_phone', '$checkin_date', '$checkin_hour', '$artist')";
+
+if ($conn->query($sql) === TRUE) {
+  //echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Затваряне на връзката към базата данни
+$conn->close();
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,10 +57,10 @@
       <br>
       <div class="bookform">
       <img src="../Booking/BookingIcon.png" class="avatar">
-        <form action="../Artists/artists.php" method="post">
+        <form action="booking.php" method="post">
             <div class="elem-group">
               <label for="name">Your Name</label>
-              <input type="text" id="name" name="visitor_name" placeholder="John Doe" pattern=[A-Z\sa-z]{3,20} required>
+              <input type="text" id="name" name="visitor_name" placeholder="John Doe">
             </div>
             <div class="elem-group">
               <label for="email">Your E-mail</label>
@@ -27,25 +68,25 @@
             </div>
             <div class="elem-group">
               <label for="phone">Your Phone</label>
-              <input type="tel" id="phone" name="visitor_phone" pattern=(\d{3})-?\s?(\d{3})-?\s?(\d{4}) required>
+              <input type="tel" id="phone" name="visitor_phone" required>
             </div>
       
             <div class="elem-group inlined">
               <div class="inf">
                  <label for="checkin-date"> Date</label>
-                 <input type="date" id="checkin-date" name="checkin" required>
+                 <input type="date" id="checkin-date" name="checkin_date" required>
               </div>
              
               <div class="inf">
                 <label for="checkin-date"> Hour</label> 
-                <input type="time" id="checkin-date" name="checkin" required>
+                <input type="time" id="checkin-hour" name="checkin_hour" required>
               </div> 
            
              
             </div>
             <div class="elem-group">
-              <label for="room-selection">Select Artist</label>
-              <select id="room-selection" name="room_preference" required>
+              <label for="artist">Select Artist</label>
+              <select id="artist" name="artist" required>
                   <option value="">None</option>
                   <option value="artist">Sabrina Wilson</option>
                   <option value="artist">Tomas Smith</option>
