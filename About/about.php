74074b8913e5ec454e72220b4467b0ba196f60a7
TsankoDokov <tsankod05g@gmail.com>
2023-05-22 17:27:53 +0300

    New About 80%

--- About/about.php
+++ About/about.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="Images/Logo1.jpg" type="image/x-icon">
     <link rel="stylesheet" href="../Nav/nav.css">
-    <link rel="shortcut icon" type= "x-icon" href="../Images/Logo.png">
     <link rel="stylesheet" type="text/css" href="aboutStyle.css">
     <title>About</title>
 </head>
@@ -14,39 +13,36 @@
     <header>
         <?php include('../Nav/nav.php'); ?>
     </header>
-   <div class="about-section">
-  <div>
-  <h1 class="white Header">About Us</h1>
+    <div class="about-section">
+        <h1 class="section-heading">About Us</h1>
 
-  <div class="container">
-  <div class="inf">
-    <div>
-    <h2 class="white">Our Mission</h2>
-    <p class="white">Our mission is to provide high-quality tattoo artistry in a comfortable and welcoming environment. We strive to make each of our clients feel valued and satisfied with their tattoo experience.</p>
-    <img class="image"  src="OurMission.jpeg" alt="IMG">
-  </div>
-  <br>
-  <div class="container">
-  <div class="inf">
-    <div>
-    <h2 class="white">Our Studio</h2>
-    <p class="white">Our studio is a clean, modern space that is designed to make our clients feel comfortable and relaxed. We use only the highest quality materials and equipment to ensure a safe and sterile environment for our clients.</p>
-    <img class="image"  src="../About/ArtistsPhoto.jpeg" alt="IMG">
+        <div class="container">
+            <div class="info">
+                <h2>Our Mission</h2>
+                <p>Our mission is to provide high-quality tattoo artistry in a comfortable and welcoming environment. We strive to make each of our clients feel valued and satisfied with their tattoo experience.</p>
+                <img class="image" src="OurMission.jpeg" alt="Our Mission">
+            </div>
+        </div>
 
-  </div>
-  <br>
-  <div class="container">
-  <div class="inf">
-    <div>
-    <h2 class="white">Our Artists</h2>
-    <p class="white">Our talented artists are dedicated to creating stunning, one-of-a-kind tattoos that reflect each client's individuality. They are passionate about their craft and always stay up-to-date with the latest techniques and trends.</p>
-  </div>
-  
-<!-- map -->
-<div> 
-  <br>
-  <h1 class="white location-text">OUR LOCATION</h1>
-  <iframe class="location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1706.385870426293!2d23.339217366207365!3d42.69042961196529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa85dc8d254ecb%3A0x8208adf4924414df!2sBloody%20Chains%2013%20Tattoo!5e0!3m2!1sbg!2sbg!4v1676833374095!5m2!1sbg!2sbg" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
+        <div class="container">
+            <div class="info">
+                <h2>Our Studio</h2>
+                <p>Our studio is a clean, modern space that is designed to make our clients feel comfortable and relaxed. We use only the highest quality materials and equipment to ensure a safe and sterile environment for our clients.</p>
+                <img class="image" src="../About/ArtistsPhoto.jpeg" alt="Our Studio">
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="info">
+                <h2>Our Artists</h2>
+                <p>Our talented artists are dedicated to creating stunning, one-of-a-kind tattoos that reflect each client's individuality. They are passionate about their craft and always stay up-to-date with the latest techniques and trends.</p>
+                <img class="image" src="../About/OurStudio.jpg" alt="Our Studio">
+            </div>
+        </div>
+    </div>
+    <!-- <div class="location-container">
+        <h1 class="location-heading">OUR LOCATION</h1>
+        <iframe class="location-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1706.385870426293!2d23.339217366207365!3d42.69042961196529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa85dc8d254ecb%3A0x8208adf4924414df!2sBloody%20Chains%2013%20Tattoo!5e0!3m2!1sbg!2sbg!4v1676833374095!5m2!1sbg!2sbg" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div> -->
 </body>
-</html>
+</html> 
