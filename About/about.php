e9f5d67e11833b575f400ed416588fa7cc1169e4
TsankoDokov <tsankod05g@gmail.com>
2023-05-23 13:18:21 +0300

    About page - 90%

--- About/about.php
+++ About/about.php
@@ -13,7 +13,7 @@
     <header>
         <?php include('../Nav/nav.php'); ?>
     </header>
-    <div class="about-section">
+    <!-- <div class="about-section">
         <h1 class="section-heading">About Us</h1>
 
         <div class="container">
@@ -40,9 +40,35 @@
             </div>
         </div>
     </div>
-    <!-- <div class="location-container">
+     <div class="location-container">
         <h1 class="location-heading">OUR LOCATION</h1>
         <iframe class="location-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1706.385870426293!2d23.339217366207365!3d42.69042961196529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa85dc8d254ecb%3A0x8208adf4924414df!2sBloody%20Chains%2013%20Tattoo!5e0!3m2!1sbg!2sbg!4v1676833374095!5m2!1sbg!2sbg" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div> -->
+    </div>  -->
+  
+    <h1>About Us</h1>
+  
+  <div class="container">
+    <section class="mission">
+      <h2><i class="fa fa-users" aria-hidden="true"></i> Our Mission</h2>
+      <p>Our mission is to provide high-quality tattoo artistry in a comfortable and welcoming environment. We strive to make each of our clients feel valued and satisfied with their tattoo experience.</p>
+    </section>
+    <section class="studio">
+      <h2><i class="fa fa-building" aria-hidden="true"></i> Our Studio</h2>
+      <p>Our studio is a clean, modern space that is designed to make our clients feel comfortable and relaxed. We use only the highest quality materials and equipment to ensure a safe and sterile environment for our clients.</p>
+    </section>
+    <section class="artists">
+      <h2><i class="fa fa-pencil" aria-hidden="true"></i> Our Artists</h2>
+      <p>Our talented artists are dedicated to creating stunning, one-of-a-kind tattoos that reflect each client's individuality. They are passionate about their craft and always stay up-to-date with the latest techniques and trends.</p>
+    </section>
+  </div>
+  <div class="location">
+      <h1><i class="fa fa-map-marker" aria-hidden="true"></i> Our Location</h1>
+      <div class="map-container">
+        <iframe class="location-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1706.385870426293!2d23.339217366207365!3d42.69042961196529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa85dc8d254ecb%3A0x8208adf4924414df!2sBloody%20Chains%2013%20Tattoo!5e0!3m2!1sbg!2sbg!4v1676833374095!5m2!1sbg!2sbg" width="100%" height="190" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+  </div>
+
+</body>
 </body>
 </html> 
